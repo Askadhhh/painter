81c6b848c3a10773cc36f3e22ccfbc0365c48b71
Askadhhh <127306364+Askadhhh@users.noreply.github.com>
2023-03-08 17:27:34 +0500

    grid setup

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const app = document.getElementById("app");
+
+Array(100)
+  .fill(null)
+  .forEach(() => {
+    const div = document.createElement("div");
+    div.classList.add("cell");
+
+    app.append(div);
+  });
+
+app.addEventListener("click", handlerClickDiv);
+
+function handlerClickDiv(event) {
+  // event.target - элемент на который кликнули
+  // .style.backgroundColor - свойство фона элемента
+  // .className - строка с именем класса у элемента
+}
